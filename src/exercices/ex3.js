f29675855619e7db5c4f0deec58872258b2fd515
pmazan <p.mazan@zeendoc.com>
2021-11-26 15:27:57 +0100

    correction consigne ex3

--- src/exercices/ex3.js
+++ src/exercices/ex3.js
@@ -1,5 +1,5 @@
 // 1) A partir des liens ci-dessous, faire un algo qui récupère via la fonction "fetch" la liste des utilisateurs et la liste des articles
-// 2) Filtrer les tâches qui n'ont pas été complétés (.completed)
+// 2) Filtrer les tâches et ne garder que les id pairs
 // 3) Transformer cette liste de taches pour ne renvoyés que le titre (.title) ainsi que le nom d'utilisateur (.username) de chaque todo
 // 4) Faire un console.log du résultat
 
