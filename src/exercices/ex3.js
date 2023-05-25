f441f3be44180e87f5f61dded0d56dd4f3465294
ThomasBONNEMAYRE <t.bonnemayre@zeendoc.com>
2023-05-25 18:31:15 +0200

    revue avec pierre

--- src/exercices/ex3.js
+++ src/exercices/ex3.js
@@ -1,14 +1,17 @@
 
 async function main() {
 
+
+    //promise.all() fetch await
+
     //1 A partir des liens ci-dessous, faire un algo qui récupère via la fonction "fetch" la liste des utilisateurs et la liste des articles
-    const responseU = await fetch("https://jsonplaceholder.typicode.com/users"),
-        users = await responseU.json(),
-        responseP = await fetch("https://jsonplaceholder.typicode.com/posts"),
-        posts = await responseP.json();
+    const responseU = await fetch("https://jsonplaceholder.typicode.com/users");
+    const  users = await responseU.json();
+    const  responseP = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const  posts = await responseP.json();
 
     //2 Filtrer les tâches et ne garder que les id pairs
-    const evenPosts = (posts.filter(o => o.hasOwnProperty("id"))).filter(o => o["id"] % 2 === 0)
+    const evenPosts = posts.filter(o => (o.id !== undefined && o.id % 2 === 0)) //potentiellement source d'erreurs silencieuses
 
     //3 Transformer cette liste de taches pour ne renvoyés que le titre (.title) ainsi que le nom d'utilisateur (.username) de chaque todo
     let finalResultBis = []
